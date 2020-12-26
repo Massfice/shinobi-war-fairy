9cbf86d397093049a6f968f74a363ec98b6e6245
Massfice <39681631+Massfice@users.noreply.github.com>
2020-12-27 00:08:20 +0100

    Feature/discord bot api (#28)

--- subforum-api/src/services/ExtractPostsService.js
+++ subforum-api/src/services/ExtractPostsService.js
@@ -1,69 +1,217 @@
 class ExtractPostsService {
     extractHtmlTag(parsedHtml) {
-        return parsedHtml.find((el => el.tagName && el.tagName === 'html'));
+        return parsedHtml.find(
+            (el) =>
+                el.tagName &&
+                el.tagName === 'html'
+        );
     }
 
     extractBodyTag(parsedHtml) {
-        return parsedHtml.children.find((el => el.tagName && el.tagName === 'body'));
+        return parsedHtml.children.find(
+            (el) =>
+                el.tagName &&
+                el.tagName === 'body'
+        );
     }
 
     extractWrap(parsedHtml) {
-        return parsedHtml.children.find((el => el.attributes && el.attributes.length > 0 && el.attributes[0].value === 'wrap'));
+        return parsedHtml.children.find(
+            (el) =>
+                el.attributes &&
+                el.attributes.length > 0 &&
+                el.attributes[0].value === 'wrap'
+        );
     }
 
     extractForumBegins(parsedHtml) {
-        return parsedHtml.children.filter((el => el.attributes && el.attributes.some(attribute => attribute.value === 'forumbegin')));
+        return parsedHtml.children.filter(
+            (el) =>
+                el.attributes &&
+                el.attributes.some(
+                    (attribute) =>
+                        attribute.value ===
+                        'forumbegin'
+                )
+        );
     }
 
     extractOdstep1as(parsedHtml) {
-        return parsedHtml.children.find((el => el.attributes && el.attributes.some(attribute => attribute.value === 'odstep1a')));
+        return parsedHtml.children.find(
+            (el) =>
+                el.attributes &&
+                el.attributes.some(
+                    (attribute) =>
+                        attribute.value ===
+                        'odstep1a'
+                )
+        );
     }
 
     extractOdstep2s(parsedHtml) {
-        return parsedHtml.children.find((el => el.attributes && el.attributes.some(attribute => attribute.value === 'odstep2')));
+        return parsedHtml.children.find(
+            (el) =>
+                el.attributes &&
+                el.attributes.some(
+                    (attribute) =>
+                        attribute.value ===
+                        'odstep2'
+                )
+        );
     }
 
     extractForum4s(parsedHtml) {
-        return parsedHtml.children.find((el => el.attributes && el.attributes.some(attribute => attribute.value === 'forum4')));
+        return parsedHtml.children.find(
+            (el) =>
+                el.attributes &&
+                el.attributes.some(
+                    (attribute) =>
+                        attribute.value ===
+                        'forum4'
+                )
+        );
     }
 
     createLastPostData(parsedHtml) {
-        const authorATag = parsedHtml.children.find((el => el.tagName === 'a' && el.attributes.some((attribute => attribute.key === 'class' && attribute.value === 'username-coloured'))));
-        const przezElParts = authorATag ? [] : parsedHtml.children.find((el => el.type === 'text' && el.content.startsWith('przez '))).content.split(' &raquo; ');
+        const authorATag = parsedHtml.children.find(
+            (el) =>
+                el.tagName === 'a' &&
+                el.attributes.some(
+                    (attribute) =>
+                        attribute.key ===
+                            'class' &&
+                        attribute.value ===
+                            'username-coloured'
+                )
+        );
+        const przezElParts = authorATag
+            ? []
+            : parsedHtml.children
+                  .find(
+                      (el) =>
+                          el.type === 'text' &&
+                          el.content.startsWith(
+                              'przez '
+                          )
+                  )
+                  .content.split(' &raquo; ');
 
-        const author = authorATag ? authorATag.children[0].content : przezElParts[0].replace('przez ', '').replace('\r\n\t\t\t\t\t', '');
-        const createdAt = parsedHtml.children[parsedHtml.children.length - 1].content.replace('\r\n\t', '');
-        const url = parsedHtml.children.find((el => el.children && el.children.some((e => e.tagName === 'img')))).attributes[0].value.replaceAll('amp;', '').replace('.', 'http://shinobi-war.kylos.pl');
+        const author = authorATag
+            ? authorATag.children[0].content
+            : przezElParts[0]
+                  .replace('przez ', '')
+                  .replace('\r\n\t\t\t\t\t', '');
+        const createdAt = parsedHtml.children[
+            parsedHtml.children.length - 1
+        ].content.replace('\r\n\t', '');
+        const url = parsedHtml.children
+            .find(
+                (el) =>
+                    el.children &&
+                    el.children.some(
+                        (e) => e.tagName === 'img'
+                    )
+            )
+            .attributes[0].value.replaceAll(
+                'amp;',
+                ''
+            )
+            .replace(
+                '.',
+                'http://shinobi-war.kylos.pl'
+            );
 
         return {
             author,
             createdAt,
             url
-        }
+        };
     }
 
     extractPosts(parsedHtml) {
         const posts = [];
 
-        const htmlTag = this.extractHtmlTag(parsedHtml);
+        const htmlTag = this.extractHtmlTag(
+            parsedHtml
+        );
 
-        const bodyTag = this.extractBodyTag(htmlTag);
+        const bodyTag = this.extractBodyTag(
+            htmlTag
+        );
 
-        const wrap = this.extractWrap(bodyTag)
+        const wrap = this.extractWrap(bodyTag);
 
-        const forumBegins = this.extractForumBegins(wrap);
+        const forumBegins = this.extractForumBegins(
+            wrap
+        );
 
         for (const forumBegin of forumBegins) {
-            const postData = this.extractOdstep1as(forumBegin);
+            const postData = this.extractOdstep1as(
+                forumBegin
+            );
 
-            const authorATag = postData.children.find((el => el.tagName === 'a' && el.attributes.some((attribute => attribute.key === 'class' && attribute.value === 'username-coloured'))));
-            const przezElParts = authorATag ? [] : postData.children.find((el => el.type === 'text' && el.content.startsWith('przez '))).content.split(' &raquo; ');
+            const authorATag = postData.children.find(
+                (el) =>
+                    el.tagName === 'a' &&
+                    el.attributes.some(
+                        (attribute) =>
+                            attribute.key ===
+                                'class' &&
+                            attribute.value ===
+                                'username-coloured'
+                    )
+            );
+            const przezElParts = authorATag
+                ? []
+                : postData.children
+                      .find(
+                          (el) =>
+                              el.type ===
+                                  'text' &&
+                              el.content.startsWith(
+                                  'przez '
+                              )
+                      )
+                      .content.split(' &raquo; ');
 
-            const title = postData.children.find((el => el.tagName === 'a')).children[0].content;
-            const numberOfPosts = parseInt(this.extractOdstep2s(forumBegin).children[0].children[0].content) + 1;
-            const author = authorATag ? authorATag.children[0].content : przezElParts[0].replace('przez ', '');
-            const createdAt = authorATag ? postData.children.find((el => el.type === 'text' && el.content.startsWith(' &raquo; '))).content.replace(' &raquo; ', '').replace('\r\n\t', '') : przezElParts[1].replace('\r\n\t', '');
-            const lastPost = this.createLastPostData(this.extractForum4s(forumBegin));
+            const title = postData.children.find(
+                (el) => el.tagName === 'a'
+            ).children[0].content;
+            const numberOfPosts =
+                parseInt(
+                    this.extractOdstep2s(
+                        forumBegin
+                    ).children[0].children[0]
+                        .content
+                ) + 1;
+            const author = authorATag
+                ? authorATag.children[0].content
+                : przezElParts[0].replace(
+                      'przez ',
+                      ''
+                  );
+            const createdAt = authorATag
+                ? postData.children
+                      .find(
+                          (el) =>
+                              el.type ===
+                                  'text' &&
+                              el.content.startsWith(
+                                  ' &raquo; '
+                              )
+                      )
+                      .content.replace(
+                          ' &raquo; ',
+                          ''
+                      )
+                      .replace('\r\n\t', '')
+                : przezElParts[1].replace(
+                      '\r\n\t',
+                      ''
+                  );
+            const lastPost = this.createLastPostData(
+                this.extractForum4s(forumBegin)
+            );
 
             posts.push({
                 title,
@@ -72,7 +220,6 @@ class ExtractPostsService {
                 createdAt,
                 lastPost
             });
-
         }
 
         return posts;
